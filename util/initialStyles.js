f102e2b0ffff3edaa91cbafca2eb103f0087c808
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-07-08 22:04:25 +0500

    initial styles for component

---
+++ util/initialStyles.js
@@ -0,0 +1,70 @@
+export const initialComponentStyles = {
+  color: "#FF0000",
+  height: "30px",
+  width: "30px",
+  backgroundColor: "#FFFFFF",
+  fontSize: "10px",
+};
+
+// const data = [
+//   {
+//     componentType: "Div",
+//     parentId: null,
+//     id: 1,
+//     styles: {
+//       color: "#FF0000",
+//       height: "30px",
+//       width: "30px",
+//       backgroundColor: "#FFFFFF",
+//       fontSize: "10px",
+//     },
+//   },
+//   {
+//     componentType: "Div",
+//     parentId: 1,
+//     id: 2,
+//     styles: {
+//       color: "#FF0000",
+//       height: "30px",
+//       width: "30px",
+//       backgroundColor: "#FFFFFF",
+//       fontSize: "10px",
+//     },
+//   },
+//   {
+//     componentType: "Div",
+//     parentId: 1,
+//     id: 3,
+//     styles: {
+//       color: "#FF0000",
+//       height: "30px",
+//       width: "30px",
+//       backgroundColor: "#FFFFFF",
+//       fontSize: "10px",
+//     },
+//   },
+//   {
+//     componentType: "Div",
+//     parentId: 1,
+//     id: 4,
+//     styles: {
+//       color: "#FF0000",
+//       height: "30px",
+//       width: "30px",
+//       backgroundColor: "#FFFFFF",
+//       fontSize: "10px",
+//     },
+//   },
+//   {
+//     componentType: "Div",
+//     parentId: 2,
+//     id: 5,
+//     styles: {
+//       color: "#FF0000",
+//       height: "30px",
+//       width: "30px",
+//       backgroundColor: "#FFFFFF",
+//       fontSize: "10px",
+//     },
+//   },
+// ];
