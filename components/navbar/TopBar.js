c97e73671d8b5df5ddfa2a84ff3d121ea56d41e8
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-07-08 21:57:23 +0500

    importing initial styles

--- components/navbar/TopBar.js
+++ components/navbar/TopBar.js
@@ -1,29 +1,10 @@
 import ComponentContext from "@/store/component-context";
 import { Container } from "@/styles/TopBar.styles";
 import { component } from "@/util/component-type";
+import { initialComponentStyles } from "@/util/initialStyles";
 import { useContext } from "react";
 
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case component.div:
-//       return { ...state, backgroundColor: action.payload };
-//     case component.button:
-//       return { ...state, color: action.payload };
-//     default:
-//       return state;
-//   }
-// };
-
-const initialState = {
-  color: "blue",
-  height: "30px",
-  width: "30px",
-  backgroundColor: "white",
-  fontSize: "10px",
-};
-
 const TopBar = () => {
-  // const [state, dispatch] = useReducer(reducer, initialState);
   const componentCtx = useContext(ComponentContext);
 
   const addComponentHandler = (event) => {
@@ -31,21 +12,17 @@ const TopBar = () => {
       case component.div:
         componentCtx.addComponent({
           componentType: component.div,
-          styles: { ...initialState },
+          styles: { ...initialComponentStyles },
         });
         break;
       case component.button:
         componentCtx.addComponent({
           componentType: component.button,
-          styles: { ...initialState },
+          styles: { ...initialComponentStyles },
         });
         break;
     }
   };
-
-  // const addComponentHandler = (event) => {
-  //   dispatch({ type: event.target.value });
-  // };
 
   return (
     <Container>
