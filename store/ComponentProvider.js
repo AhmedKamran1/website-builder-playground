fffc3e8bbd68a5b0422a853c0cfa92bb21c6d0f2
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-07-10 13:04:12 +0500

    added functionalities for manipulation

--- store/ComponentProvider.js
+++ store/ComponentProvider.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ComponentContext from "./component-context";
 
 const ComponentProvider = (props) => {
   const [componentData, setComponentData] = useState([]);
   const [componentId, setComponentId] = useState(null);
+  const [componentType, setComponentType] = useState(null);
+
+  const selectedComponentType = () => {
+    if (componentData.length != 0) {
+      const component = componentData.find(
+        (component) => component.id === componentId
+      );
+      setComponentType(component.componentType);
+    }
+  };
 
   const addComponent = (component) => {
     setComponentData((prevData) => [...prevData, component]);
@@ -13,20 +23,27 @@ const ComponentProvider = (props) => {
     setComponentId(id);
   };
 
-  const updateComponent = (styles) => {
+  const updateComponent = (styles, extraFunctionalities) => {
     if (componentData.length != 0) {
       const clonedComponentData = structuredClone(componentData);
       const updateStyleIndex = clonedComponentData.findIndex(
         (component) => component.id === componentId
       );
       clonedComponentData[updateStyleIndex].styles = styles;
+      clonedComponentData[updateStyleIndex].extraFunctionalities =
+        extraFunctionalities;
       setComponentData(clonedComponentData);
     }
   };
 
+  useEffect(() => {
+    selectedComponentType();
+  }, [componentId]);
+
   return (
     <ComponentContext.Provider
       value={{
+        componentType,
         componentData,
         addComponent,
         selectedComponent,
