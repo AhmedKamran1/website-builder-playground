962800809150ba3221d0398d0ac202db2a969719
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-07-10 12:55:41 +0500

    updated styling and updated functionality

--- components/navbar/TopBar.js
+++ components/navbar/TopBar.js
@@ -1,35 +1,47 @@
 import ComponentContext from "@/store/component-context";
-import { Container } from "@/styles/TopBar.styles";
+import * as NavStyles from "../../styles/navbar/TopBar";
+import * as ComponentStyles from "../../styles/button/button";
 import { component } from "@/util/component-type";
-import { initialComponentStyles } from "@/util/initialStyles";
 import { useContext } from "react";
+import { initialButtonComponentStyles } from "@/styles/pre-defined-components-styles/button";
 
 const TopBar = () => {
   const componentCtx = useContext(ComponentContext);
 
-  const addComponentHandler = (event) => {
+  const addComponentHandler = (event, styles) => {
     switch (event.target.textContent) {
       case component.div:
         componentCtx.addComponent({
           componentType: component.div,
-          styles: { ...initialComponentStyles },
+          styles: { ...styles },
+          extraFunctionalities: {},
         });
         break;
       case component.button:
         componentCtx.addComponent({
           componentType: component.button,
-          styles: { ...initialComponentStyles },
+          styles: { ...styles },
+          extraFunctionalities: {},
         });
         break;
     }
   };
 
   return (
-    <Container>
-      <button onClick={addComponentHandler}>div</button>
-      <br />
-      <button onClick={addComponentHandler}>button</button>
-    </Container>
+    <NavStyles.TopBarGridContainer item xs={12}>
+      {/* <button onClick={()=>addComponentHandler}>div</button> */}
+      {/* <br /> */}
+      {initialButtonComponentStyles.map((styles, index) => (
+        <ComponentStyles.StyledButton
+          variant="contained"
+          key={index}
+          {...styles}
+          onClick={(event) => addComponentHandler(event, styles)}
+        >
+          button
+        </ComponentStyles.StyledButton>
+      ))}
+    </NavStyles.TopBarGridContainer>
   );
 };
 
