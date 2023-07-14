15515a58bbf84878aa313269813f953534dae108
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-07-14 16:01:18 +0500

    added redux persist

--- store/ComponentSlice.js
+++ store/ComponentSlice.js
@@ -4,37 +4,50 @@ const componentInitialState = {
   componentData: [],
   selectedComponentId: null,
   selectedComponentType: null,
+  selectedComponentDetails: null,
+  componentId: 1,
 };
-
-let counter = 1;
 
 const componentSlice = createSlice({
   name: "Component",
   initialState: componentInitialState,
   reducers: {
     addComponent(state, action) {
-      state.componentData.push({ id: counter++, ...action.payload });
+      state.componentData.push({ id: state.componentId++, ...action.payload });
     },
 
     selectedComponent(state, action) {
+      state.selectedComponentDetails = action.payload.componentData;
       state.selectedComponentId = action.payload.componentId;
       state.selectedComponentType = action.payload.componentType;
     },
     updateComponent(state, action) {
-      if (state.componentData.length != 0) {
+      if (
+        state.componentData.length != 0 &&
+        state.selectedComponentId != null
+      ) {
         const updateStyleIndex = state.componentData.findIndex(
           (component) => component.id === state.selectedComponentId
         );
-        state.componentData[updateStyleIndex].styles =
-          action.payload.modifiedStyles;
+        state.componentData[updateStyleIndex].styles = {
+          ...state.componentData[updateStyleIndex].styles,
+          ...action.payload.modifiedStyles,
+        };
         state.componentData[updateStyleIndex].extraFunctionalities =
           action.payload.extraFunctionalities;
+        state.selectedComponentDetails.styles =
+          state.componentData[updateStyleIndex].styles;
+
+        state.selectedComponentDetails.extraFunctionalities =
+          state.componentData[updateStyleIndex].extraFunctionalities;
       }
     },
   },
 });
 
 export const componentData = (state) => state.component.componentData;
+export const selectedComponentData = (state) =>
+  state.component.selectedComponentDetails;
 export const componentId = (state) => state.component.selectedComponentId;
 export const componentType = (state) => state.component.selectedComponentType;
 
--- store/store.js
+++ store/store.js
@@ -1,11 +1,42 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import componentSlice from "./ComponentSlice";
+
 import { createWrapper } from "next-redux-wrapper";
 
-const store = () =>
-  configureStore({
-    reducer: { component: componentSlice.reducer },
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "component",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, componentSlice.reducer);
+
+const makeStore = () => {
+  const store = configureStore({
+    reducer: { component: persistedReducer },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   });
 
-export const wrapper = createWrapper(store);
+  store.__persistor = persistStore(store);
+  return store;
+};
+
+export const wrapper = createWrapper(makeStore);
 export const componentActions = componentSlice.actions;
