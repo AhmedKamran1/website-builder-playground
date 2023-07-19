83dca11eaadc87fd0fb5be2b084f6b41eb14bbdc
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-07-20 01:46:58 +0500

    added navbar event types

--- util/event-types.js
+++ util/event-types.js
@@ -1,4 +1,14 @@
-export const eventType = {
+export const commonEventType = {
+  SETINITIALSTATE: "setInitialState",
+  CHANGECOLOR: "changeColor",
+  CHANGEBGCOLOR: "changeBgColor",
+  CHANGEFONTWEIGHT: "changeFontWeight",
+  CHANGEFONTSTYLE: "changeFontStyle",
+  CHANGEHOVERCOLOR: "changeHoverColor",
+  CHANGEFONTFAMILY: "changeFontFamily",
+};
+
+export const buttonEventType = {
   SETINITIALSTATE: "setInitialState",
   CHANGECOLOR: "changeColor",
   CHANGEHEIGHT: "changeHeight",
@@ -14,5 +24,20 @@ export const eventType = {
   CHANGEHOVERCOLOR: "changeHoverColor",
   CHANGEINNERTEXT: "changeInnerText",
   CHANGEFONTFAMILY: "changeFontFamily",
-  CHANGEBUTTONMODE: "changeButtonMode"
+};
+
+export const navEventTypes = {
+  SETINITIALNAVSTATE: "setInitialState",
+  CHANGECOLOR: "changeColor",
+  CHANGEBGCOLOR: "changeBgColor",
+  CHANGEREDIRECTLINK1: "changeRedirectLink1",
+  CHANGEREDIRECTLINK2: "changeRedirectLink2",
+  CHANGEREDIRECTLINK3: "changeRedirectLink3",
+  CHANGEREDIRECTLINK4: "changeRedirectLink4",
+  CHANGEREDIRECTLINK5: "changeRedirectLink5",
+  CHANGEINNERTEXT1: "changeInnerText1",
+  CHANGEINNERTEXT2: "changeInnerText2",
+  CHANGEINNERTEXT3: "changeInnerText3",
+  CHANGEINNERTEXT4: "changeInnerText4",
+  CHANGEINNERTEXT5: "changeInnerText5",
 };
