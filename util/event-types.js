85534b3e295cee426bf32180559e11dee8262f8d
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-07-23 02:30:28 +0500

    fixed event types

--- util/event-types.js
+++ util/event-types.js
@@ -26,14 +26,8 @@ export const buttonEventType = {
 
 export const navEventTypes = {
   SETINITIALNAVSTATE: "setInitialState",
-  CHANGEREDIRECTLINK1: "changeRedirectLink1",
-  CHANGEREDIRECTLINK2: "changeRedirectLink2",
-  CHANGEREDIRECTLINK3: "changeRedirectLink3",
-  CHANGEREDIRECTLINK4: "changeRedirectLink4",
-  CHANGEREDIRECTLINK5: "changeRedirectLink5",
-  CHANGEINNERTEXT1: "changeInnerText1",
-  CHANGEINNERTEXT2: "changeInnerText2",
-  CHANGEINNERTEXT3: "changeInnerText3",
-  CHANGEINNERTEXT4: "changeInnerText4",
-  CHANGEINNERTEXT5: "changeInnerText5",
+  CHANGENAVLINK:"changeNavLink",
+  CHANGENAVINNERTEXT:"changeNavInnerText",
+  CHANGENAVDROPDOWNLINK: "changeNavDropDownLink",
+  CHANGENAVDROPDOWNINNERTEXT: "changeNavDropDownInnertext"
 };
