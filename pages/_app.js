1238011c504bd89590077de04d4852e733cccd54
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-07-26 23:16:08 +0500

    added mui theme

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,20 @@ import { wrapper } from "@/store/store";
 import { Provider } from "react-redux";
 
 import { PersistGate } from "redux-persist/integration/react";
+import { ThemeProvider } from "styled-components";
+import { createTheme } from "@mui/material";
+import theme from "@/styles/theme/theme";
 
+const customTheme = createTheme(theme());
 
 function App({ Component, pageProps, ...rest }) {
   const { store } = wrapper.useWrappedStore(rest);
   return (
     <Provider store={store}>
       <PersistGate loading={<h1>Loading</h1>} persistor={store.__persistor}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={customTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   );
