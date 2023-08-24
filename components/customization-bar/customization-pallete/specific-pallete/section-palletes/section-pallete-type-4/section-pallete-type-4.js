4d902a88c6c4a1b9b84760e1d05b76bcce6534cc
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-08-25 03:47:58 +0500

    button functionalities added

--- components/customization-bar/customization-pallete/specific-pallete/section-palletes/section-pallete-type-4/section-pallete-type-4.js
+++ components/customization-bar/customization-pallete/specific-pallete/section-palletes/section-pallete-type-4/section-pallete-type-4.js
@@ -4,30 +4,75 @@ import SectionGridButton from "../common-section-palletes/SectionGridButton";
 import { gridTextItem } from "@/helpers/pre-defined-components-styles/section-styles";
 import { SectionEventTypesEnum } from "@/helpers/constants/event-types/event-types";
 import DeleteItem from "../../../common-palletes/DeleteItem";
-import { Divider } from "@mui/material";
+import { Checkbox, Divider } from "@mui/material";
 import AddItem from "../../../common-palletes/AddItem";
 import SectionCardStyles from "../common-section-palletes/SectionCardStyles";
 import CommonButtonPallete from "../../../common-palletes/CommonButtonPallete";
 import SectionTextArea from "../common-section-palletes/SectionTextArea";
+import SectionButton from "../common-section-palletes/SectionButton";
+import ImagePallete from "../../../common-palletes/ImagePallete";
 
 const SectionPallete4 = ({
   textFunctionalities,
+  buttonFunctionalities,
+  imageFunctionalities,
   dispatchTextActions,
+  dispatchImageActions,
   cardStyles,
   buttonStyles,
   debounceSectionCardStylesActions,
   debounceSectionButtonStylesActions,
+  dispatchButtonFunctionalities,
 }) => {
   return (
     <>
+      {/* <ImagePallete
+        imageFunctionalitiesState={imageFunctionalities}
+        imageStylesState={imageStylesState}
+        dispatchImageFunctionalities={dispatchImageActions}
+        dispatchImageStyles={debounceSectionImageStylesActions}
+        changeImage={
+          SectionEventTypesEnum.sectionImageFunctionalities.CHANGEIMAGE
+        }
+        changeImageRedirectLink={
+          SectionEventTypesEnum.sectionImageFunctionalities
+            .CHANGEIMAGEREDIRECTLINK
+        }
+        changeImageAltText={
+          SectionEventTypesEnum.sectionImageFunctionalities.CHANGEIMAGEALTTEXT
+        }
+        changeImageBorderRadius={
+          SectionEventTypesEnum.imageStyles.CHANGEBORDERRADIUS
+        }
+      /> */}
       <SectionCardStyles
         cardStyles={cardStyles}
         debounceSectionCardStylesActions={debounceSectionCardStylesActions}
       />
-      <CommonButtonPallete
-        state={buttonStyles}
-        dispatch={debounceSectionButtonStylesActions}
-      />
+      <div>
+        <strong>Show Button</strong>
+        <Checkbox
+          checked={buttonFunctionalities.showButton}
+          onChange={() =>
+            dispatchButtonFunctionalities({
+              type: SectionEventTypesEnum.sectionButtonFunctionalities
+                .CHANGEBUTTONVISIBILITY,
+            })
+          }
+        />
+      </div>
+      {buttonFunctionalities.showButton && (
+        <>
+          <CommonButtonPallete
+            state={buttonStyles}
+            dispatch={debounceSectionButtonStylesActions}
+          />
+          <SectionButton
+            state={buttonFunctionalities}
+            dispatch={dispatchButtonFunctionalities}
+          />
+        </>
+      )}
       <SectionTextArea
         state={textFunctionalities}
         dispatch={dispatchTextActions}
