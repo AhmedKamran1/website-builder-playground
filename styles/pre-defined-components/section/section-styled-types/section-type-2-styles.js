9def01f51db926119d7d55ef2e4531b5123e0e4f
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-08-25 03:45:56 +0500

    updating styling issues

--- styles/pre-defined-components/section/section-styled-types/section-type-2-styles.js
+++ styles/pre-defined-components/section/section-styled-types/section-type-2-styles.js
@@ -3,7 +3,7 @@ import { Box, Container } from "@mui/material";
 import { styledWithoutProps } from "../../ignoredProps";
 
 export const SectionContainer2 = styledWithoutProps(Container, ({backgroundColor}) => ({
-  minHeight: "300px",
+  // minHeight: "250px",
   // border: "1px solid red",
   display: "flex",
   flexDirection: "column",
@@ -19,7 +19,6 @@ export const SectionTextBox2 = styledWithoutProps(Box, ({}) => ({
   flexDirection: "column",
   gap: "20px",
   width:"100%",
-  maxWidth: "700px",
-  minHeight: "200px",
+  maxWidth: "750px",
   // border: "1px solid yellow",
 }));
