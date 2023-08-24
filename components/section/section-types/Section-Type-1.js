c73eb656044f6a36a1c011b4f5bc0886f53fdec5
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-08-25 03:06:07 +0500

    updated styles to grid layout

--- components/section/section-types/Section-Type-1.js
+++ components/section/section-types/Section-Type-1.js
@@ -5,10 +5,11 @@ import {
 import {
   SectionContainer1,
   SectionImageBox1,
+  SectionItemContainer1,
   SectionTextBox1,
 } from "@/styles/pre-defined-components/section/section-styled-types/section-type-1-styles";
 import { formatText } from "@/util/format-text";
-import { Avatar, Box } from "@mui/material";
+import { Avatar, Box, Container, Grid } from "@mui/material";
 import React from "react";
 
 const SectionType1 = ({
@@ -28,47 +29,59 @@ const SectionType1 = ({
       isfocused={isfocused}
       maxWidth={false}
       backgroundColor={blockStyles.backgroundColor}
-      flexDirection={blockStyles.flexDirection}
     >
-      <SectionTextBox1>
-        {headingText && (
-          <Box textAlign={headingStyles.textAlign}>
-            <ResponsiveText
-              variant="header"
-              colorHex={headingStyles.colorHex}
-              fontStyle={headingStyles.fontStyle}
-              fontWeight={headingStyles.fontWeight}
-            >
-              {formatText(headingText)}
-            </ResponsiveText>
-          </Box>
-        )}
-        {paragraphText && (
-          <Box textAlign={paragraphStyles.textAlign}>
-            <ResponsiveText
-              variant="mainBody"
-              colorHex={paragraphStyles.colorHex}
-              fontStyle={paragraphStyles.fontStyle}
-              fontWeight={paragraphStyles.fontWeight}
-            >
-              {formatText(paragraphText)}
-            </ResponsiveText>
-          </Box>
-        )}
-      </SectionTextBox1>
-      <SectionImageBox1>
-        <StyledLink href={imageRedirectLink}>
-          <Avatar
-            src={image}
-            alt={imageAltText}
-            sx={{
-              height: "100%",
-              width: "100%",
-              borderRadius: `${imageStyles.borderRadius}%`,
-            }}
-          />
-        </StyledLink>
-      </SectionImageBox1>
+      <Container maxWidth="md">
+        <Grid
+          container
+          justifyContent="center"
+          spacing={2.5}
+          flexDirection={blockStyles.flexDirection}
+        >
+          <Grid item xs={8} lg={6}>
+            <SectionTextBox1>
+              {headingText && (
+                <Box textAlign={headingStyles.textAlign}>
+                  <ResponsiveText
+                    variant="header"
+                    colorHex={headingStyles.colorHex}
+                    fontStyle={headingStyles.fontStyle}
+                    fontWeight={headingStyles.fontWeight}
+                  >
+                    {formatText(headingText)}
+                  </ResponsiveText>
+                </Box>
+              )}
+              {paragraphText && (
+                <Box textAlign={paragraphStyles.textAlign}>
+                  <ResponsiveText
+                    variant="mainBody"
+                    colorHex={paragraphStyles.colorHex}
+                    fontStyle={paragraphStyles.fontStyle}
+                    fontWeight={paragraphStyles.fontWeight}
+                  >
+                    {formatText(paragraphText)}
+                  </ResponsiveText>
+                </Box>
+              )}
+            </SectionTextBox1>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <SectionImageBox1>
+              <StyledLink href={imageRedirectLink} style={{ width: "70%" }}>
+                <Avatar
+                  src={image}
+                  alt={imageAltText}
+                  sx={{
+                    height: "100%",
+                    width: "100%",
+                    borderRadius: `${imageStyles.borderRadius}%`,
+                  }}
+                />
+              </StyledLink>
+            </SectionImageBox1>
+          </Grid>
+        </Grid>
+      </Container>
     </SectionContainer1>
   );
 };
--- styles/pre-defined-components/section/section-styled-types/section-type-1-styles.js
+++ styles/pre-defined-components/section/section-styled-types/section-type-1-styles.js
@@ -1,34 +1,30 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 
 import { styledWithoutProps } from "../../ignoredProps";
 
 export const SectionContainer1 = styledWithoutProps(
   Container,
-  ({ flexDirection, backgroundColor, theme }) => ({
-    minHeight: "350px",
-    // border: "1px solid red",
+  ({ backgroundColor, theme }) => ({
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: "30px",
-    padding:"2rem",
-    flexDirection: flexDirection,
+    flexDirection: "column",
+    padding: "2rem",
     backgroundColor: backgroundColor,
     [theme.breakpoints.down("md")]: {
-      flexDirection: flexDirection === "row" ? "column" : "column-reverse",
-      gap:"10px"
+      padding: "2rem 0",
     },
   })
 );
 
 export const SectionImageBox1 = styledWithoutProps(Box, ({ theme }) => ({
-  width:"100%",
-  maxWidth: "350px",
   display: "flex",
+  height: "200px",
   justifyContent: "center",
 }));
 
-export const SectionTextBox1 = styledWithoutProps(SectionImageBox1, ({}) => ({
+export const SectionTextBox1 = styledWithoutProps(Box, ({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  minHeight: "200px",
   flexDirection: "column",
-  gap:"15px"
+  gap: "15px",
 }));
